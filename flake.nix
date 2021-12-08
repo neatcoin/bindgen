a67606185777872883d954de12e9b447ac16754a
Wei Tang <wei@that.world>
2021-12-08 12:31:54 +0000

    Make nix flake build work

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
-  inputs.nixpkgs.url = "github.com:nixos/nixpkgs/nixos-21.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
     packages."x86_64-linux"."neatcoin-bindgen" = let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in with pkgs; yarn2nix-moretea.mkYarnWorkspace {
+    in with pkgs; yarn2nix-moretea.mkYarnPackage {
       name = "neatcoin-bindgen-0.1.0";
       src = ./.;
       packageJSON = ./package.json;
       yarnLock = ./yarn.lock;
-      packageOverrides = {
-        neatcoin-bindgen = {
-          buildPhase = ''
-            yarn run build
-          '';
-        };
-      }
+      buildPhase = ''
+        yarn run build
+        echo -e "#!/usr/bin/env node\n$(cat ./deps/@neatcoin/bindgen/dist/index.js)" > ./deps/@neatcoin/bindgen/dist/index.js 
+        chmod +x ./deps/@neatcoin/bindgen/dist/index.js
+      '';
     };
   };
 }
