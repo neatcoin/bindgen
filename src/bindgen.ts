809abbde5a0b1cca3225825be3fbff565ff0c8d8
Wei Tang <wei@that.world>
2021-12-08 21:38:26 +0000

    Add line break for zone files

--- src/bindgen.ts
+++ src/bindgen.ts
@@ -159,7 +159,7 @@ export async function run() {
     const zonePath = path.join(destination, `${zoneName}.zone`);
     zoneFiles[zoneName].unshift("@ SOA vodka.dns.neatcoin.org. postmaster.that.world. (0 21600 3600 604800 86400)");
     zoneFiles[zoneName].unshift("@ NS vodka.dns.neatcoin.org.");
-    await writeFile(zonePath, zoneFiles[zoneName]);
+    await writeFile(zonePath, zoneFiles[zoneName].join("\n"));
   }
 
   const namedPath = path.join(destination, "named.conf");
